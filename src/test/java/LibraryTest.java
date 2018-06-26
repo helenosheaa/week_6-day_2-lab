1b4a057ad26798d014476fa02427163e5c4964fe
Gregor Cox <gregorcox@gmail.com>
2018-06-26 15:13:02 +0100

    initial commit

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library books;
+
+    @Before
+    public void before(){
+        ArrayList<String> testBooks = new ArrayList<>();
+        testBooks.add("book1");
+        testBooks.add("book2");
+        testBooks.add("book3");
+        testBooks.add("book4");
+
+        books = new Library(testBooks);
+    }
+
+    @Test
+    public void hasNumberOfEntries(){
+        assertEquals(4, books.getBookCount());
+    }
+}
