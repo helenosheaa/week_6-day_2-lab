c34db79d773fe3f0126f1b623dd5ccfcaa5b6dc4
helenosheaa <helenosheaa@gmail.com>
2018-06-27 08:24:20 +0100

    adding a string to genretype

--- src/main/java/GenreType.java
+++ src/main/java/GenreType.java
@@ -1,6 +1,16 @@
 public enum GenreType {
-    THRILLER,
-    CHILDREN,
-    CRIME,
-    ROMANTIC
+    THRILLER("Scary"),
+    CHILDREN("Playful"),
+    CRIME("Intriguing"),
+    ROMANTIC("Love");
+
+    private final String value;
+
+    GenreType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 }
